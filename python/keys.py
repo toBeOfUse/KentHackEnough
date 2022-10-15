7956a8e13356d14c616c4ffd80504a857045f1b8
toBeOfUse <mitchjacov@gmail.com>
2022-10-15 15:04:38 -0400

    python one to one key mapping

---
+++ python/keys.py
@@ -0,0 +1,24 @@
+keymap: dict[str, str] = {}
+
+def setKeyMapPair(key1: str, key2: str):
+    keymap[key1] = key2
+    keymap[key2] = key1
+    keymap[key1.lower()] = key2.lower()
+    keymap[key2.lower()] = key1.lower()
+
+setKeyMapPair('A', ';');
+setKeyMapPair('B', 'B');
+setKeyMapPair('C', 'M');
+setKeyMapPair('D', 'K');
+setKeyMapPair('E', 'O');
+setKeyMapPair('F', 'J');
+setKeyMapPair('G', 'H');
+setKeyMapPair('I', 'R');
+setKeyMapPair('L', 'S');
+setKeyMapPair('N', 'V');
+setKeyMapPair('P', 'W');
+setKeyMapPair('Q', '[');
+setKeyMapPair('T', 'U');
+setKeyMapPair('X', ',');
+setKeyMapPair('Y', 'Y');
+setKeyMapPair('Z', '.');
