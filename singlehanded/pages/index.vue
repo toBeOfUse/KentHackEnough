3965774b5ec068b0e5eecd1864e370a0fcc5877c
toBeOfUse <mitchjacov@gmail.com>
2022-10-16 07:40:15 -0400

    revise index page content

--- singlehanded/pages/index.vue
+++ singlehanded/pages/index.vue
@@ -3,20 +3,20 @@
     <h1 class="text-3xl my-4">One-Handed Typing for the Masses</h1>
     <p>
       Hello. A month ago, after breaking my arm, I suddenly found typing a lot
-      more difficult. There were surprisingly few resources available online to
-      help me.
+      more difficult. Naturally, I started scouring the Internet for resources
+      that would help me.
     </p>
     <p>
+      I honestly didn't find a lot that was for free, but I did come across
       <a
         href="https://blog.xkcd.com/2007/08/14/mirrorboard-a-one-handed-keyboard-layout-for-the-lazy/"
         target="_blank"
-        >This</a
+        >this</a
       >
-      blog post from Randall Munroe of xkcd inspired me to implement a keyboard
-      layout that lets you switch what keys are available to you on the fly,
-      letting you reach the whole alphabet with the fingers of just one hand,
-      including a configuration file that lets you use it with some Linux input
-      methods.
+      blog post from Randall Munroe of xkcd, describing and providing a Linux
+      XInput configuration file for a new, dynamic keyboard layout; one that
+      switches keys around in response to your input, putting the whole universe
+      of letters into the reach of one single hand.
       <em
         >The goal of this website is to bring this functionality to the
         masses.</em
@@ -24,15 +24,20 @@
     </p>
     <p>
       The crux of this input method is this:
-      <strong>while you hold space, the keyboard is mirrored.</strong> This
-      allows you to seamlessly switch your muscle memory so that your left pinky
-      can take over from your right pinky, or vice versa, and for the other
-      fingers too. <em>It's less weird than it sounds.</em>
+      <strong
+        >while you hold space, the letters of your keyboard are
+        mirrored.</strong
+      >
+      This allows you to seamlessly switch over your muscle memory so that your
+      left pinky can take over from your right pinky, or vice versa, and the
+      same for the other kinds of fingers too.
+      <em>It's less weird than it sounds.</em> To get a sense of it, you have a
+      few different options:
     </p>
     <ul class="list-disc px-8">
-      <li><a href="/wordprocessor">Try it in-browser.</a></li>
+      <li><a href="/wordprocessor">Try it right here, in your browser.</a></li>
       <li>
-        <a href="/tutorial">Learn with the tutorial.</a>
+        <a href="/tutorial">Get tutored, by the tutorial.</a>
       </li>
       <li>
         <a href="/fungame">Play the Fun Game.</a>
@@ -44,6 +49,10 @@
         >
       </li>
     </ul>
+    <p class="opacity-70 italic text-sm">
+      This project was created in 24 hours by Mitch for the Kent State
+      University "Kent Hack Enough" hackathon.
+    </p>
   </div>
 </template>
 
